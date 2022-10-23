d6b948bf8ae5ba35a0e1ed80aac7d7015a272042
loidtayag <loidtayag@gmail.com>
2022-10-23 14:37:40 +0100

    Reset

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,20 +1,6 @@
 const express = require("express");
 const cors = require("cors")
 const app = express()
-const path = require("path")
-
-// Heroku environment
-// "start": "react-scripts start",
-// "start": "react-scripts start",
-const port = process.env.PORT || 5000;
-// app.use("/", express.static("build"))
-app.get("*", (req, res) => {
-  res.sendFile(__dirname.replace("src\\backend", "build\\index.html"))
-})
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"))
-}
 
 // Middleware
 app.use(cors({
@@ -24,5 +10,5 @@ app.use(cors({
 // Routes
 app.use("/", require("./routers/boardsRouter"));
 
-app.listen(port);
+app.listen(5000);
 
--- src/index.tsx
+++ src/index.tsx
@@ -16,3 +16,25 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
+
+/* Heroku trials, ended bad XD */
+//    "start": "ts-node-esm src/index.tsx",
+//    "heroku-postbuild": "npm install && npm run build"
+
+//  "type": "module"
+
+// "proxy": "http://localhost:5000"
+
+// Heroku environment
+// const port = process.env.PORT || 5000;
+// // app.use("/", express.static("build"))
+// app.get("*", (req, res) => {
+//   res.sendFile(__dirname.replace("src\\backend", "build\\index.html"))
+// })
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"))
+// }
+
+//Procfile contents
+//web: npm start
